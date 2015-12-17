32556af213ffaba403144c27f97f16cb67156d30
tyler <happydooby@gmail.com>
2015-12-17 18:57:49 -0500

    Ranking button now working

--- src/main/resources/web/js/main.js
+++ src/main/resources/web/js/main.js
@@ -64,7 +64,7 @@ function fillSelects(data) {
   $(data).each(function() {
     console.log(this);
     $('#select_candidate_' + this.id).attr('oldValue', '');
-    $('#select_candidate_' + this.id).attr('newValue', '6');
+    $('#select_candidate_' + this.id).attr('newValue', '');
   });
 }
 
@@ -73,16 +73,20 @@ function setupSelects(data) {
 
   fillSelects(data);
 
+  setupRankButton(data);
 
+  setupRankNextButton(data);
+
+}
+
+function setupRankButton(data) {
 
   // Now if the selects are changed, resort, and make them distinct
 
   $(".select_candidate").on('change', function(e) {
 
-
-
     // console.log(e);
-    console.log(this.id);
+    // console.log(this.id);
     // console.log(this);
 
     var changedId = this.id.split('_').slice(-1)[0];
@@ -91,12 +95,9 @@ function setupSelects(data) {
     $('#select_candidate_' + changedId).attr('newValue', newValue);
 
 
-
-
-    console.log('ov = ' + oldValue + ' cv = ' + newValue);
+    // console.log('ov = ' + oldValue + ' cv = ' + newValue);
 
     // set that as the selected option
-    // $('#select_candidate_' + changedId).val(newValue);
     $('#select_candidate_' + changedId).attr('oldValue', newValue);
 
     // Loop over all the other selects, and remove that option
@@ -111,9 +112,7 @@ function setupSelects(data) {
       }
 
 
-
       if (oldValue != "") {
-        console.log('got here');
 
         // loop over all the others and this old one back in.
         var option = $('<option></option>').attr("value", oldValue).text(oldValue);
@@ -121,7 +120,6 @@ function setupSelects(data) {
         $(data).each(function() {
           if (id != changedId) {
             $('#select_candidate_' + id).append(option);
-            // now sort them
           }
         });
       }
@@ -129,11 +127,7 @@ function setupSelects(data) {
       // Sort the options correctly
       $('#select_candidate_' + id).sort_select_box();
 
-
-
     });
-
-
 
     // Sort the table
     sortTableCustom('#ballot_table');
@@ -141,10 +135,88 @@ function setupSelects(data) {
   });
 
 
-
-  // <option name="{{name}}" value="{{id}}">{{name}}</option>
 }
 
+var cRank = 1;
+
+function setupRankNextButton(data) {
+  $('.rank_next_candidate').click(function() {
+    var id = this.id.split('_').splice(-1)[0];
+    var rank = $(this).attr('rank');
+    var rankNext = $(this);
+
+    // console.log(id);
+    console.log(rank);
+
+
+
+    if (rank > 0) {
+      $(this).addClass('btn-default');
+      $(this).removeClass('btn-success');
+      var oldRank = $(this).attr('rank');
+      $(this).attr('rank', 0);
+      $('#select_candidate_' + id).val('').change();
+      // change all with a greater rank, 
+      // move them down one
+
+      var rows = $('#ballot_table').children('tbody').children('tr');
+
+      var difference = cRank - oldRank - 1;
+      console.log('difference = ' + difference);
+
+      var maxRank = 0;
+      $(rows).each(function() {
+        // console.log(this);
+
+        var rowRank = parseInt($(this).find('td:nth-child(2) select :selected').text());
+        var rowId = $(this).find('td:nth-child(1) button').attr('id').split('_').slice(-1)[0];
+        // console.log(rowRank);
+        // console.log(rowId);
+
+        var newRowRank = parseInt(rowRank);
+
+        // Set those values
+        if (rowRank > rank) {
+
+          newRowRank--;
+          // console.log('Setting rank for id = ' + this.id + ' rank = ' + newRowRank);
+          $(this).find('td:nth-child(1) button').attr('rank', newRowRank);
+          $(this).find('td:nth-child(2) select').val(newRowRank).change();
+          // $(this).find('td:nth-child(2) select').val(newRowRank);
+
+        } else {
+          $('#rank_next_candidate_' + id).attr('rank', 0);
+        }
+
+        // Set the new max cRank
+        if (maxRank < newRowRank) {
+          maxRank = newRowRank;
+        }
+
+        cRank = maxRank + 1;
+
+
+        // console.log(maxRank);
+        // console.log(cRank);
+
+      });
+
+
+
+
+
+    }
+
+    // Set the val for the select to the cRank
+    else {
+      $(this).removeClass('btn-default');
+      $(this).addClass('btn-success');
+      $(this).attr('rank', cRank);
+      $('#select_candidate_' + id).val(cRank++).change();
+    }
+
+  });
+}
 
 
 function sortTableCustom(tableName) {
@@ -152,22 +224,20 @@ function sortTableCustom(tableName) {
   var table = $(tableName);
   var id = table.attr('id');
 
-  // Create a clone of the rows
+  // Fetch the rows
   var rows = table.children('tbody').children('tr');
 
 
   rows.sort(function(a, b) {
     var a = parseInt($(a).find('td:nth-child(2) select :selected').text(), 10);
     var b = parseInt($(b).find('td:nth-child(2) select :selected').text(), 10);
-    // console.log(a);
-    // console.log(b);
 
     // setting the NaNs to end up at the bottom
     if (isNaN(a)) {
-      a = 999999;
+      a = Number.MAX_VALUE;
     }
     if (isNaN(b)) {
-      b = 999999;
+      b = Number.MAX_VALUE;
     }
 
     if (a == b) return 0;
@@ -175,55 +245,8 @@ function sortTableCustom(tableName) {
   });
 
   // Do the sorting
-  for (var i = 0; i < rows.length; i++){
-  	table.append(rows[i])
+  for (var i = 0; i < rows.length; i++) {
+    table.append(rows[i]);
   }
-
-
-  // console.log(rows);
-
-  // // var sortedTbody = $('<tbody id="ballot_body"></tbody>');
-  // // var oldTbody = document.getElementById("ballot_body");
-  // // var sortedTbody = document.createElement('tbody');
-  // var sortedTbody = $('<div></div>');
-
-  // $(rows).each(function() {
-
-  //   console.log(this);
-
-  //   var select = $(this).find('select');
-  //   select.val(select.newValue);
-
-  //   console.log(select);
-  //   console.log(select.newValue);
-
-  //   sortedTbody.append(this);
-
-  // });
-
-  // console.log(sortedTbody);
-  // console.log(allRows);
-  // console.log(oldTbody);
-
-  // oldTbody.parentNode.replaceChild(sortedTbody, oldTbody);
-
-  // $(tableName + " tr").remove();
-
-
-  // console.log(rows.html());
-  // replace the rows with the sorted rows
-  // $(this).find('tbody').empty();
-  // console.log(document.getElementById("ballot_body").innerHTML);
-  // console.log(sortedTbody.html());
-
-  // document.getElementById("ballot_body").innerHTML = sortedTbody.html();
-
-
-  // #ballot_table > tbody:nth-child(2)
-  // $(rows).each(function() {
-  //   console.log(this);
-  //   $("#" + id + ' tr:last').after(this);
-  // });
-
 
 }
