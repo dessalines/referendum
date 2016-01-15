7c06b232c37371c42488184cbf85326d378e0e7e
tyler <happydooby@gmail.com>
2016-01-15 14:57:58 -0500

    Saving

--- src/main/resources/web/js/test.js
+++ src/main/resources/web/js/test.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
 
-  console.log('got here');
-  // With JQuery
 
-  var obj = '#ex1';
+  initializeAllRangeVotes();
+
+  $('[data-toggle="tooltip"]').tooltip({
+    container: 'body'
+  });
+
+
+});
+
+function initializeAllRangeVotes() {
+  $('.range_vote').each(function() {
+    var cId = '#' + this.id;
+    setupRangeVote(cId);
+  });
+}
+
+function setupRangeVote(obj) {
+
+  // With JQuery
   var slider = $(obj).bootstrapSlider({
-      reversed: true
+      reversed: true,
+      tooltip: 'show'
     })
     .on('slide', function() {
       RGBChange(obj);
@@ -13,15 +30,28 @@ $(document).ready(function() {
     .on('slideStop', function() {
       console.log('done voting');
       $(obj).attr('vote', true);
+      $(obj + '_vote').removeClass('hide');
     });
 
   initializeSlider(obj);
-  setupUnVote(obj);
+  setupClearVote(obj);
+  setupThumbs(obj);
+  
 
-  $('[data-toggle="tooltip"]').tooltip({
-    container: 'body'
-  });
-});
+
+}
+
+function setupThumbs(obj) {
+
+	// Hide slider and clear by default
+	$(obj + '_slider'  + ',' + obj + '_clear_vote').addClass('hide');
+
+	// Unhide slider and clear
+	$(obj + '_vote').click(function() {
+		$(obj + '_slider' + ',' + obj + '_clear_vote').toggleClass('hide');
+		$(obj + '_vote').addClass('hide');
+	});
+}
 
 function initializeSlider(obj) {
   $(obj).attr('vote', false);
@@ -31,25 +61,32 @@ function initializeSlider(obj) {
 function RGBChange(obj) {
 
   // convert the value to 0-255
+
   var val = $(obj).bootstrapSlider('getValue');
-  var correctId = obj + 'Slider';
+  // $(obj).bootstrapSlider('setAttribute','tooltip','show');
+  // console.log($(obj).bootstrapSlider('getAttribute','tooltip'));
+
+  var correctId = obj + '_slider';
 
   var calc = Math.floor(val * 255 / 10);
   var redVal = 255 - calc;
   var greenVal = calc;
 
   // console.log(redVal);
-  $(correctId + ' .slider-track-high').css('background', 'rgb(' + redVal + ',' + greenVal + ',' + 0 + ')');
+  var selector = $(correctId).find('.slider-track-high');
+  // console.log(selector);
+  $(selector).css('background', 'rgb(' + redVal + ',' + greenVal + ',' + 0 + ')');
 };
 
-function setupUnVote(obj) {
-  $('#unvote').click(function() {
-      $(obj).bootstrapSlider('setValue', 5);
-      $(obj + 'Slider .slider-track-high').css('background', '#BABABA');
-      $(obj).attr('vote', false);
+function setupClearVote(obj) {
+  $(obj + '_clear_vote').click(function() {
+    var obj = '#' + $(this).parent().find('input').attr('id');
+    console.log(obj);
+    $(obj).bootstrapSlider('setValue', 5);
+    $(obj + '_slider .slider-track-high').css('background', '#BABABA');
+    $(obj).attr('vote', false);
 
-
-    })
-    // $(obj).bootstrapSlider('setValue', '5.01');
-    // console.log($(obj).bootstrapSlider('getValue'));
+  });
+  // $(obj).bootstrapSlider('setValue', '5.01');
+  // console.log($(obj).bootstrapSlider('getValue'));
 }
